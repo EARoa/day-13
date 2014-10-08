c99da5b602628e3dae0027dd39fd0628383d26a4
Efrain Ayllon <jesusayllonroa@gmail.com>
2014-10-08 12:12:40 -0500

    F'IN PANDAS

---
+++ panda.rb
@@ -0,0 +1,12 @@
+class Panda
+
+
+  def eat(food)
+    %i(bamboo pikas rodent).include? food
+  end
+
+  def climb(that_with_which_is_climbed)
+    that_with_which_is_clmbed == :true
+  end
+  
+end
